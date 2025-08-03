cc2f5e72538407c09cfb37921eabbbe58eb6f915
Imane Ismail <86661348+Imane-Ismail@users.noreply.github.com>
2025-08-03 10:51:36 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='loghawk',
-    version='1.4',
+    version='1.0',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'pyfiglet',
+        'pandas',
+        'colorama',
+        'regex'
     ],
     entry_points={
         'console_scripts': [
-            'loghawk=loghawk.loghawk_cli:main',
+            'loghawk=loghawk.loghawk_cli:main'
         ],
     },
-    author='Imane Ismail',
-    description='A lightweight log analysis tool for cyber defenders',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-    ],
 )
